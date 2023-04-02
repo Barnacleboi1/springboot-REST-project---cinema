47ff84976ec876ec69db04917054c54fd90880f9
Robert <robert.cuda@email.cz>
2023-04-02 21:40:04 +0200

    aaa

--- src/cinema/Cinema.java
+++ src/cinema/Cinema.java
@@ -5,29 +5,29 @@ import java.util.*;
 
 
 public class Cinema {
-    private int total_rows;
-    private int total_columns;
-    private List<Seat> available_seats;
+    private int totalRows;
+    private int totalColumns;
+    private List<Seat> availableSeats;
 
     public Cinema(int rows, int columns) {
-        this.total_rows = rows;
-        this.total_columns = columns;
-        this.available_seats = new ArrayList<>();
-        for (int i = 1; i < total_rows + 1; i++) {
-            for (int j = 1; j < total_columns + 1; j++) {
-                available_seats.add(new Seat(i, j));
+        this.totalRows = rows;
+        this.totalColumns = columns;
+        this.availableSeats = new ArrayList<>();
+        for (int i = 1; i < totalRows + 1; i++) {
+            for (int j = 1; j < totalColumns + 1; j++) {
+                availableSeats.add(new Seat(i, j));
             }
         }
     }
 
-    public int getTotal_rows() {
-        return total_rows;
+    public int getTotalRows() {
+        return totalRows;
     }
-    public int getTotal_columns() {
-        return total_columns;
+    public int getTotalColumns() {
+        return totalColumns;
     }
 
-    public List<Seat> getAvailable_seats() {
-        return available_seats;
+    public List<Seat> getAvailableSeats() {
+        return availableSeats;
     }
 }
