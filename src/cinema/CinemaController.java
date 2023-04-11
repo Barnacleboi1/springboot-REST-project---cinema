f0c2d8f18b503adbc268bb67f900f3215d545cf2
Robert <robert.cuda@email.cz>
2023-04-11 13:56:22 +0200

    edits

--- src/cinema/CinemaController.java
+++ src/cinema/CinemaController.java
@@ -1,14 +1,13 @@
 package cinema;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
 import java.util.Map;
 
-import static cinema.ErrorMSG.*;
 
 @RestController
 public class CinemaController {
--- src/cinema/Ticket.java
+++ src/cinema/Ticket.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
 import java.util.Objects;
-import java.util.UUID;
 
 public class Ticket {
     @JsonPropertyOrder({"token", "seat"})
